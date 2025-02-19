05f4187cf7f6030f827bd602373506b01f2b9d2d
Germán Santos <crashbito@gmail.com>
2025-02-18 22:38:17 -0600

    Create basicos.swift

---
+++ basicos.swift
@@ -0,0 +1,22 @@
+// Básicos
+// Declaración formal de un dato
+
+var edad: Int = 18
+print(edad)
+// Restamos el valor de 3 a nuestra variable
+edad -= 3
+print(edad) // el valor que veremos en pantalla será de 15
+
+// Uso de if-else
+// Ejemplo de ver si un valor es par o impar
+if edad % 2 == 0{
+  print("Es par")
+}
+else{  
+  print("es impar")
+}
+
+// Elementos de comparación y/o evaluación
+// > < == != ! <= >=
+
+
